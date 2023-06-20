60b83c2e8914ad367f7eccc51ee28af60ac9fdbd
22yn0113 <134387056+22yn0113@users.noreply.github.com>
2023-06-20 20:31:44 +0900

    Add files via upload

---
+++ EX15.php
@@ -0,0 +1,22 @@
+<?php
+    // 入力された値を受け取り、計算結果を表示する
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>EX15　yyJNccnn　四則演算の入力フォーム(受信)</title>
+</head>
+<body>
+
+
+
+
+
+</body>
+</html>
